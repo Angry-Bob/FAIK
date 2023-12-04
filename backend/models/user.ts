282e0c172197f1302c3fe7efdadf9add053230d8
Robert <rcastor0000@gmail.com>
2023-12-04 11:44:09 -0600

    models added

---
+++ backend/models/user.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+  role: {type: String,required: true,},
+  content: {type: String,required: true,},
+});
+
+const conversationSchema = new mongoose.Schema({
+    conversation: [messageSchema]
+})
+
+const userSchema = new mongoose.Schema({
+  name: {type: String, required: true,},
+  email: {type: String,required: true,unique: true,},
+  password: {type: String,required: true,},
+  chats: [conversationSchema],
+});
+
+export default mongoose.model("User", userSchema);
